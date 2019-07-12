68638735cfd2a0a5905c9e2a1d232325bc73f286
Tkwsm <kawashima38@gmail.com>
2019-07-12 12:45:02 +0900

    modified test_molcalc.rb for adduct related commands

--- test_molcalc.rb
+++ test_molcalc.rb
@@ -1,3 +1,4 @@
+
 require 'test/unit'
 require './molcalc.rb'
 
@@ -50,8 +51,6 @@ class TC_ClassAtomlist < Test::Unit::TestCase
     assert_equal(["C","Cl","H","N","O"],  @ca.chop_mol_formula("C26H19Cl2N3O7").keys.sort )
     assert_equal( 1,  @ca.chop_mol_formula( "Fe" )["Fe"] )
     assert_equal( 2,  @ca.chop_mol_formula( "CH3COOH" )["C"] )
-    assert_equal( 2,  @ca.chop_mol_formula( "CH3COOH" )["O"] )
-    assert_equal( 4,  @ca.chop_mol_formula( "CH3COOH" )["H"] )
   end
 end
 
@@ -112,6 +111,62 @@ class TC_MolCalc < Test::Unit::TestCase
     assert_equal( 145.9118, @mc.exact_molecular_mass("H2SeO4").round(4) )
     assert_equal( 555.0600, @mc.exact_molecular_mass("C26H19Cl2N3O7").round(4))
     assert_equal( 55.9349, @mc.exact_molecular_mass("Fe").round(4))
+  end
+
+  def test_adduct_H
+    assert_equal( 1.0072764319, @mc.adduct_H )
+  end
+
+  def test_adduct_mw
+    #             [sign, Mass, charge, mult]
+    assert_equal(["+",  1.007276, 3.0, 1.0 ], @mc.adduct_mw("[M+3H]3+", 6))
+    assert_equal(["+",  8.33459, 3.0, 1.0 ], @mc.adduct_mw("[M+2H+Na]3+", 5))
+    assert_equal(["+", 22.9892, 3.0, 1.0], @mc.adduct_mw("[M+3Na]3+", 4))
+    assert_equal(["+", 1.007276, 2.0, 1.0], @mc.adduct_mw("[M+2H]2+", 6))
+    assert_equal(["+", 9.520551, 2.0, 1.0], @mc.adduct_mw("[M+H+NH4]2+", 6))
+    assert_equal(["+", 11.99825, 2.0, 1.0], @mc.adduct_mw("[M+H+Na]2+", 5))
+    assert_equal(["+", 19.98522, 2.0, 1.0], @mc.adduct_mw("[M+H+K]2+", 5))
+    assert_equal(["+", 22.98922, 2.0, 1.0], @mc.adduct_mw("[M+2Na]2+", 5))
+    assert_equal(["+", 22.98922, 1.0, 1.0], @mc.adduct_mw("[M+Na]+", 5))
+    assert_equal(["+", 38.963158, 1.0, 1.0], @mc.adduct_mw("[M+K]+", 6))
+    assert_equal(["+", 61.0653, 1.0, 1.0], @mc.adduct_mw("[M+IsoProp+H]+", 4))
+    assert_equal(["+", 83.0604, 1.0, 1.0], @mc.adduct_mw("[M+2ACN+H]+", 4))
+    assert_equal(["+", 22.98922, 1.0, 2.0], @mc.adduct_mw("[2M+Na]+", 5))
+    assert_equal(["+", 38.963158, 1.0, 2.0], @mc.adduct_mw("[2M+K]+", 6))
+    assert_equal(["+", 64.01577, 1.0, 1.0], @mc.adduct_mw("[M+ACN+Na]+", 6))
+    assert_equal(["+", 64.01577, 1.0, 2.0], @mc.adduct_mw("[2M+ACN+Na]+", 6))
+    assert_equal(["+", 62.547100, 2.0, 1.0], @mc.adduct_mw("[M+3ACN+2H]2+", 6))
+    assert_equal(["+", 21.52055, 2.0, 1.0], @mc.adduct_mw("[M+ACN+2H]2+", 5))
+    assert_equal(["+", 76.91904, 1.0, 1.0], @mc.adduct_mw("[M+2K-H]+", 6))
+    assert_equal(["+", 33.03349, 1.0, 1.0], @mc.adduct_mw("[M+CH3OH+H]+", 5))
+    assert_equal(["+", 42.0338, 1.0, 2.0], @mc.adduct_mw("[2M+ACN+H]+", 4))  #
+    assert_equal(["+", 42.0338, 2.0, 1.0], @mc.adduct_mw("[M+2ACN+2H]2+", 4))#
+    assert_equal(["+", 42.0338, 1.0, 1.0], @mc.adduct_mw("[M+ACN+H]+", 4))   #
+    assert_equal(["+", 18.0338, 1.0, 1.0], @mc.adduct_mw("[M+NH4]+", 4))
+    assert_equal(["+", 1.007276, 1.0, 1.0], @mc.adduct_mw("[M+H]+", 6))
+##    assert_equal(["+", 15.766190, 3.0, 1.0 ], @mc.adduct_mw("[M+H+2Na]3+", 6))
+    assert_equal(["+", 44.97117, 1.0, 1.0], @mc.adduct_mw("[M+2Na-H]+", 5))
+    assert_equal(["+", 79.0212, 1.0, 1.0], @mc.adduct_mw("[M+DMSO+H]+", 4))
+##    assert_equal(["+", 84.05511, 1.0, 1.0], @mc.adduct_mw("[M+IsoProp+Na+H]+", 6))
+    assert_equal(["+", 1.007276, 1.0, 2.0], @mc.adduct_mw("[2M+H]+", 6))
+    assert_equal(["+", 18.0338, 1.0, 2.0], @mc.adduct_mw("[2M+NH4]+", 4)) #
+
+    assert_equal(["-", -1.007276, -3.0, 1.0], @mc.adduct_mw("[M-3H]3-", 6)) #
+    assert_equal(["-", -1.007276, -2.0, 1.0], @mc.adduct_mw("[M-2H]2-", 6)) #
+#    assert_equal(["-", -19.01839, -2.0, 1.0], @mc.adduct_mw("[M-H2O-H]-", 6)) #
+    assert_equal(["-", -1.007276, -1.0, 1.0], @mc.adduct_mw("[M-H]-", 6)) #
+    assert_equal(["-", 20.97467, -1.0, 1.0], @mc.adduct_mw("[M+Na-2H]-", 5)) #
+    assert_equal(["-", 34.969402, -1.0, 1.0], @mc.adduct_mw("[M+Cl]-", 6)) #
+    assert_equal(["-", 36.94861, -1.0, 1.0], @mc.adduct_mw("[M+K-2H]-", 5)) #
+    assert_equal(["-", 44.99820, -1.0, 1.0], @mc.adduct_mw("[M+FA-H]-", 5)) #
+    assert_equal(["-", 59.01385, -1.0, 1.0], @mc.adduct_mw("[M+Hac-H]-", 5)) #
+    assert_equal(["-", 78.918885, -1.0, 1.0], @mc.adduct_mw("[M+Br]-", 6)) #
+    assert_equal(["-", 112.98559, -1.0, 1.0], @mc.adduct_mw("[M+TFA-H]-", 5)) #
+    assert_equal(["-", -1.007276, -1.0, 2.0], @mc.adduct_mw("[2M-H]-", 6)) #
+    assert_equal(["-", 44.99820, -1.0, 2.0], @mc.adduct_mw("[2M+FA-H]-", 5)) #
+    assert_equal(["-", 59.01385, -1.0, 2.0], @mc.adduct_mw("[2M+Hac-H]-", 5)) #
+    assert_equal(["-", -1.007276, -1.0, 3.0], @mc.adduct_mw("[3M-H]-", 6)) #
+
   end
 
 end
